0df367c123b308982e4322071794765b16bc4226
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-06-03 23:20:44 -0300

    feat: Added fallback to initials style when image fail

--- src/lib/Avatar.svelte
+++ src/lib/Avatar.svelte
@@ -12,16 +12,22 @@
 
   const abbr = initials || i(name);
   const abbrLength = abbr.length;
+
+  let imageFail = false;
+  let imageLoading = true;
 </script>
 
 <style>
   .wrapper {
+    position: relative;
     width: var(--size);
     height: var(--size);
   }
 
   .innerImage,
-  .innerInitials {
+  .innerInitials,
+  .imageLoading {
+    display: block;
     width: 100%;
     height: 100%;
     border-radius: var(--borderRadius);
@@ -32,7 +38,31 @@
     background-color: var(--bgColor);
     color: var(--textColor);
     text-align: center;
-    font-size: calc(var(--size) / (var(--abbrLenght) + 0.5));
+    font-size: calc(var(--size) / (var(--abbrLength) + 0.5));
+  }
+
+  .imageLoading {
+    background-color: var(--bgColor);
+  }
+  .imageLoading::before {
+    content: '';
+    display: block;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(90deg, transparent, #ffffff38, transparent);
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: skeleton-animation 1.2s linear infinite;
+  }
+
+  @keyframes skeleton-animation {
+    0% {
+      transform: translateX(-100%);
+    }
+    100% {
+      transform: translateX(100%);
+    }
   }
 </style>
 
@@ -40,9 +70,16 @@
   aria-label={name}
   class="wrapper"
   style="{style}--borderRadius:{borderRadius}; --size:{size}; --bgColor:{bgColor};
-  --src:{src}; --textColor:{textColor}; --abbrLenght:{abbrLenght}">
-  {#if src}
-    <img alt={name} class="innerImage" {src} />
+  --src:{src}; --textColor:{textColor}; --abbrLength:{abbrLength}">
+  {#if src && !imageFail}
+    <div class={imageLoading ? 'imageLoading' : ''}>
+      <img
+        alt=""
+        class={`innerImage`}
+        {src}
+        on:error={() => (imageFail = true)}
+        on:load={() => (imageLoading = false)} />
+    </div>
   {:else}
     <div class="innerInitials">{abbr}</div>
   {/if}
